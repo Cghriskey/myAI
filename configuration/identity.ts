f8b0d64ee040ab0d55ffe0a86ed71e4b42882534
Cghriskey <charlieghriskey@gmail.com>
2025-02-24 15:13:12 -0500

    updated owner description

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `Charlie Ghriskey is currently a second-year MBA student at UNC Kenan-Flagler Business School.`;
+export const OWNER_DESCRIPTION: string = `Charlie Ghriskey is currently a second-year MBA student at UNC Kenan-Flagler Business School. He did his undergrad studies at The College of Charleston where he majored in Business Administration.`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
