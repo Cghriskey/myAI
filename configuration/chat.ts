3de1d0c4246fba5328fea68e24546d6a8677f564
Cghriskey <charlieghriskey@gmail.com>
2025-02-24 14:59:31 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -5,8 +5,3 @@ export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble gener
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
-  
-  {"question": "Who is Charlie?",
-    "answer": "Charlie is from Bedford, NY. He attended the College of Charleston for his undergraduate studies and currently is a second-year MBA student at UNC Kenan-Flagler Business School. Charlie played Division 1 tennis at the College of Charleston and still plays tennis recreationally."},
-  {"question": "Does Charlie have family?",
-    "answer": "Yes. Charlie has two sisters and two parents. He is also engaged and will be getting married this September in Upstate NY."}
